c6cce9648004779d1ad865cf8d6ff817840a999d
david <you@example.com>
2025-11-10 21:13:52 +0800

    add *

--- middleware.ts
+++ middleware.ts
@@ -4,10 +4,10 @@ export default function middleware(request: Request) {
     const url = new URL(request.url);
 
     if (url.pathname.startsWith('/auth')) {
-        return rewrite(new URL('https://vercel-auth-rouge.vercel.app', request.url));
+        return rewrite(new URL('https://vercel-auth-rouge.vercel.app/', request.url));
     }
 
     if (url.pathname.startsWith('/biz')) {
-        return rewrite(new URL('https://vercel-business.vercel.app', request.url));
+        return rewrite(new URL('https://vercel-business.vercel.app/', request.url));
     }
 }
